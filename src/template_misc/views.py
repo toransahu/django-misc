03aeb1c4f8c3c8c834c57358e56192678df301e6
Toran Sahu <toran.sahu@yahoo.com>
2019-05-20 23:57:19 +0530

    access python dict via javascript in django template

--- src/template_misc/views.py
+++ src/template_misc/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+import json
 # Create your views here.
+
 
 def foo(request):
     context = {
-        "a": 1,
-        "b": 2
+            # "a": {"x": 3, "y": 4},
+            "a": json.dumps({"x": 3, "y": 4}),
+            "c": {"x": 5, "y": 6},
+            "b": 2
     }
+
+    context = {}
+
+    context["jd"] = json.dumps({"x": 1, "y": 2})
+    context["dd"] = {"a": 1}
     # return render(request, template_name='template_misc/hello.html', context=context)
+    # return render(request, 'template_misc/hello.html', context)
     return render(request, 'template_misc/hello.html', context)
